4be17d05c6966aebb057d0ccb5b411c88bb192bd
Andrew Haines <andrew@haines.org.nz>
2018-10-17 11:03:47 +0100

    🆙 envy v0.4.0

--- Casks/envy.rb
+++ Casks/envy.rb
@@ -1,6 +1,6 @@
 cask "envy" do
-  version "0.3.0"
-  sha256 "abece33600a711fb7dc101400b5e5b0857049aaa3426515f9019dde0a2cf2829"
+  version "0.4.0"
+  sha256 "d70ca50487ca891c16a30c962d213064d4c440db20301a2ed8d14db388e7a28d"
 
   url "https://github.com/haines/envy/releases/download/v#{version}/envy-darwin-amd64"
   homepage "https://github.com/haines/envy"
